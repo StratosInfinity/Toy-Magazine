37375da441bcfe16e3e56d2307835350f0160710
StratosInfinity <101989572+StratosInfinity@users.noreply.github.com>
2022-12-02 00:48:18 +0300

    Add files via upload

---
+++ ToyMagazineWPF/Toy Magazine/Busket.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Toy_Magazine
+{
+    /// <summary>
+    /// Логика взаимодействия для Busket.xaml
+    /// </summary>
+    public partial class Busket : Window
+    {
+        public Busket()
+        {
+            InitializeComponent();
+        }
+
+        private void Reference_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Для просмотра корзины вам нужно войти в аккаунт!", "", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+
+        private void GoProfile_Click(object sender, RoutedEventArgs e)
+        {
+            Profile profileWindow = new Profile();
+            profileWindow.Show();
+            this.Close();
+        }
+
+        private void ExitApp_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            this.Hide();
+           Profile profile = new Profile();
+            profile.Show();
+        }
+    }
+}
---
+++ ToyMagazineWPF/Toy Magazine/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Toy_Magazine
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Enter_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Такого пользователя не существует", "", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+             
+        }
+
+        private void GusetEnter_Click(object sender, RoutedEventArgs e)
+        {
+                Profile profileWindow = new Profile();
+                profileWindow.Show();
+                this.Close();
+            
+        }
+
+        private void ExitApp_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
